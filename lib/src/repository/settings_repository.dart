212e01c39cca075ba46221be8b541f2325056363
tarun <tarunplay3551@gmail.com>
2021-10-06 00:58:14 +0530

    done

--- lib/src/repository/settings_repository.dart
+++ lib/src/repository/settings_repository.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:mvc_pattern/mvc_pattern.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ import 'package:global_configuration/global_configuration.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
+import 'package:markets/src/controllers/home_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../helpers/custom_trace.dart';
@@ -19,25 +21,29 @@ import '../models/setting.dart';
 
 ValueNotifier<Setting> setting = new ValueNotifier(new Setting());
 ValueNotifier<Address> deliveryAddress = new ValueNotifier(new Address());
-Coupon coupon = new Coupon.fromJSON({
-
-});
+Coupon coupon = new Coupon.fromJSON({});
 final navigatorKey = GlobalKey<NavigatorState>();
 
 Future<Setting> initSettings() async {
   Setting _setting;
-  final String url = '${GlobalConfiguration().getString('api_base_url')}settings';
+  final String url =
+      '${GlobalConfiguration().getString('api_base_url')}settings';
   try {
-    final response = await http.get(url, headers: {HttpHeaders.contentTypeHeader: 'application/json'});
-    if (response.statusCode == 200 && response.headers.containsValue('application/json')) {
+    final response = await http
+        .get(url, headers: {HttpHeaders.contentTypeHeader: 'application/json'});
+    if (response.statusCode == 200 &&
+        response.headers.containsValue('application/json')) {
       if (json.decode(response.body)['data'] != null) {
         SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('settings', json.encode(json.decode(response.body)['data']));
+        await prefs.setString(
+            'settings', json.encode(json.decode(response.body)['data']));
         _setting = Setting.fromJSON(json.decode(response.body)['data']);
         if (prefs.containsKey('language')) {
           _setting.mobileLanguage.value = Locale(prefs.get('language'), '');
         }
-        _setting.brightness.value = prefs.getBool('isDark') ?? false ? Brightness.dark : Brightness.light;
+        _setting.brightness.value = prefs.getBool('isDark') ?? false
+            ? Brightness.dark
+            : Brightness.light;
         setting.value = _setting;
         // ignore: invalid_use_of_visible_for_testing_member, invalid_use_of_protected_member
         setting.notifyListeners();
@@ -52,44 +58,94 @@ Future<Setting> initSettings() async {
   return setting.value;
 }
 
-Future<Address> setCurrentLocation() async {
+Future<void> setCurrentLocation() async {
   print("setcurrentlocatiom");
   var location = new Location();
   MapsUtil mapsUtil = new MapsUtil();
   final whenDone = new Completer();
-  Address _address = new Address();
-  location.requestService().then((value) async {
-    print("locationserrr");
-    location.getLocation().then((_locationData) async {
-      print("getloco00");
-      print(_locationData.latitude);
-      print(_locationData.toString());
-      String _addressName = await mapsUtil.getAddressName(new LatLng(_locationData?.latitude, _locationData?.longitude), setting.value.googleMapsKey);
-      print("addrename");
-      print(_addressName);
-      _address = Address.fromJSON({'address': _addressName, 'latitude': _locationData?.latitude, 'longitude': _locationData?.longitude});
-      print(_address.toMap());
-      await changeCurrentLocation(_address);
-      whenDone.complete(_address);
-    }).timeout(Duration(seconds: 10), onTimeout: () async {
-      await changeCurrentLocation(_address);
-      whenDone.complete(_address);
-      return null;
-    }).catchError((e) {
-      whenDone.complete(_address);
+  Address address = new Address();
+  location.getLocation().then((_locationData) async {
+    print("getloco00");
+    print(_locationData.latitude);
+    print(_locationData.toString());
+    String _addressName = await mapsUtil.getAddressName(
+        new LatLng(_locationData?.latitude, _locationData?.longitude),
+        setting.value.googleMapsKey);
+    print("addrename");
+    print(_addressName);
+    address = Address.fromJSON({
+      'address': _addressName,
+      'latitude': _locationData?.latitude,
+      'longitude': _locationData?.longitude
     });
+    print(address.toMap());
+    print(address.latitude);
+    deliveryAddress.value = address;
+    deliveryAddress.notifyListeners();
+    // if(address.latitude != null){
+    //   print("ifof when done");
+    //   return address;
+    // }
+    // HomeController _con;
+    //
+    // _con.listenForTopMarkets();
+    // _con.listenForSlides();
+    // _con.listenForTrendingProducts();
+    // _con.listenForCategories();
+    // _con.listenForPopularMarkets();
+    // _con.listenForRecentReviews();
+    // _con.listenForAllMarkets();
+    await changeCurrentLocation(address);
+    print("changecurrentlocationnnn");
+    print("whendone");
+
+    //return whenDone.complete(address);
+    // return whenDone.future;
+  }).whenComplete(() async {
+    print("When Complete" + deliveryAddress.value.toMap().toString());
+    await changeCurrentLocation(address);
+
+    return whenDone.future;
+    // return deliveryAddress.value;
+  }).timeout(Duration(seconds: 10), onTimeout: () async {
+    await changeCurrentLocation(address);
+    whenDone.complete(address);
+    return null;
+  }).catchError((e) {
+    whenDone.complete(address);
   });
+  // location.requestService().then((value) async {
+  //   print("locationserrr");
+  //
+  // });
   print("location get0");
+  print(address.latitude);
   print(whenDone.isCompleted);
-  print(_address);
-  // return _address;
-  return whenDone.future;
+  print(address);
+  // return address;
+  // return whenDone.complete;
+  // if(whenDone.isCompleted){
+  //   print("ifof when done");
+  //   return address;
+
+  return address;
+  // return whenDone.future;
 }
 
 Future<Address> changeCurrentLocation(Address _address) async {
+  print("changecurrentlocation");
   if (!_address.isUnknown()) {
+    print("addddd");
+    print("homehome");
+    print("afeter adddd");
+    print(json.encode(_address.toMap()));
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString('delivery_address', json.encode(_address.toMap()));
+    await getCurrentLocation();
+
+    HomeController _ = HomeController();
+
+    print("afeter shareed pref");
   }
   return _address;
 }
@@ -98,7 +154,8 @@ Future<Address> getCurrentLocation() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   //await prefs.clear();
   if (prefs.containsKey('delivery_address')) {
-    deliveryAddress.value = Address.fromJSON(json.decode(prefs.getString('delivery_address')));
+    deliveryAddress.value =
+        Address.fromJSON(json.decode(prefs.getString('delivery_address')));
     return deliveryAddress.value;
   } else {
     deliveryAddress.value = Address.fromJSON({});
